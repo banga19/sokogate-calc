4f2c767f9c25dbf9a83726eb48433a746fd8beab
Banga19 <bangalifof16@gmail.com>
2026-04-24 16:28:07 +0300

    updated app.js to run on a different port

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const path = require('path');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
